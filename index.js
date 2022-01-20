524dd6251ac11a14b23c8faa4294d205424d7c8c
Samantha <samanthaltl.sfh@gmail.com>
2022-01-20 15:46:27 -0800

    wip

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
+require('dotenv').config()
+
 const express = require('express')
 const server = express()
 
-const PORT = 9000;
+server.use(express.json())
+
+const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () =>{
     console.log(`*~*~* listening on port ${PORT} *~*~*`)
