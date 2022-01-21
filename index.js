a8e7ce02d995cb6977696634aaf4b79fdc108a05
Samantha <samanthaltl.sfh@gmail.com>
2022-01-20 16:06:16 -0800

    added endpoint

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ const server = express()
 
 server.use(express.json())
 
+server.get('/hello', (req, res) => {
+    res.json({ message: 'hello'})
+})
+
+server.get('/', (req, res) => {
+    res.send(`
+        <h1>Welcome to Sammi's deployed app!</h1>
+    `)
+})
+
 const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, () =>{
     console.log(`*~*~* listening on port ${PORT} *~*~*`)
-})
-
-server.get('/hello', (req, res) => {
-    res.json({ message: 'hello'})
 })
